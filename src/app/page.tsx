bb1d9a4966c4247c5492cfb80a43d0e0383981fa
Leandro Savio <prorootinfo@gmail.com>
2025-04-14 12:16:36 +0100

    New text component Mercadao

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { Context } from '@/components/Context'
 import Footer from '@/components/Footer'
 import Header from '@/components/Header'
+import { Mercadao } from '@/components/Mercadao'
 import { News } from '@/components/News'
 import Player from '@/components/Player'
 import Contact from '@/components/contact'
@@ -28,6 +29,7 @@ export default function Home() {
       <Network />
       <News />
       <Contact />
+      <Mercadao />
       <Footer />
     </>
   )
---
+++ src/components/Mercadao.tsx
@@ -0,0 +1,27 @@
+export function Mercadao() {
+  return (
+    <>
+      <div>
+        <button type="button">Cadastro</button>
+        <input type="Pesquisa" />
+      </div>
+      <div>
+        <h1 className="m-4">CAMOS COMEÇAR NOSOS ESTUDOS</h1>
+        <strong className="text-white text-xl">
+          Vamos falar sobre técnologia e analisar tudos os requisitos para se
+          tornar uma bom profissional de T.I, isso não quer dizer que vai
+          trabalhar somente na área de desenvovimento de software, mas estar
+          ligado em um mundo de novidades, estou aqui propondo para você que
+          como eu é apaixonado por o mundo tech, vamos juntos fazer um mundo
+          melhor, onde podemos entregar as pessoas que tem vontade de mudar sua
+          vida para sempre e de agora em diante mudar como se pode ver o mundo
+          de uma forma com mais oportunidades para todos. Os processos que
+          envolvem o setor de T.I atualmente tem uma grande demanda de
+          profissionais que tem o compromisso de entregar as melhores formas de
+          se manter conectados com as pessoas e seus projetos pessoais para que
+          a comunidade dev venha para trazer mais divercidade no
+        </strong>
+      </div>
+    </>
+  )
+}
